517024c1f1ec2a727f47245bc7167c072e73d379
manishshukla00 <132373386+manishshukla00@users.noreply.github.com>
2024-03-14 00:40:42 +0530

    Add files via upload

--- src/customer/pages/homePage/HomePage.jsx
+++ src/customer/pages/homePage/HomePage.jsx
@@ -3,6 +3,7 @@ import MainCarousel from "../../components/HomeCarousel/MainCarousel";
 import HomeSectionCard from "../../components/homeSectionCard/HomeSectionCard";
 import { mens_kurta } from "../../../data/ecommerce-products-data/Men/men_kurta";
 import { sareePage1 } from "../../../data/ecommerce-products-data/Saree/page1";
+import { mensShoesPage1 } from "../../../data/ecommerce-products-data/shoes";
 import Footer from "../../components/Footer/Footer";
 
 const HomePage = () => {
@@ -10,7 +11,7 @@ const HomePage = () => {
     <div>
       <MainCarousel />
       <HomeSectionCard data={mens_kurta} sectionName="Mens Kurta" />
-      <HomeSectionCard data={mens_kurta} sectionName="Mens Shoes" />
+      <HomeSectionCard data={mensShoesPage1} sectionName="Mens Shoes" />
       <HomeSectionCard data={mens_kurta} sectionName="Mens Shirt" />
       <HomeSectionCard data={sareePage1} sectionName="Women's Saree" />
       <HomeSectionCard data={sareePage1} sectionName="Women's Dress" />
